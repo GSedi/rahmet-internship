cf404805d3efb32e4838a0495993dffa1bccdb42
GSedi <sedi.gamidov@gmail.com>
2019-01-09 19:00:17 +0600

    initial

---
+++ catalog-api/database/migrations/2019_01_09_100700_create_products_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('category_id');
+
+            $table->unsignedInteger('owner_id');
+
+            $table->string('name');
+
+            $table->text('description');
+
+            $table->string('color');
+
+            $table->string('price');
+
+            $table->string('weight');
+
+            $table->timestamps();
+
+            $table->foreign('owner_id')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
