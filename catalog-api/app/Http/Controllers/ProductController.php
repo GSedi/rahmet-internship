15e479bede4d1a668fb57d487e67cc7766bd4ac0
GSedi <sedi.gamidov@gmail.com>
2019-01-11 21:05:18 +0600

    filters added

--- catalog-api/app/Http/Controllers/ProductController.php
+++ catalog-api/app/Http/Controllers/ProductController.php
@@ -24,10 +24,40 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $products = collect();
 
-        return response()->json(Product::all());
+        if($request->has('color')){
+
+            $products->push(Product::where('color', 'LIKE', '%'.$request['color'].'%')->get());
+        }
+
+        if($request->has('up_price') && $request->has('low_price')){
+
+            $products->push(Product::all()->whereBetween('price', [$request['low_price'], $request['up_price']]));
+        }
+        elseif($request->has('price'))
+        {
+            $products->push(Product::all()->where('price', '<=', $request['price']));
+        }
+
+        if($request->has('up_weight') && $request->has('low_weight')){
+
+            $products->push(Product::all()->whereBetween('weight', [$request['low_weight'], $request['up_weight']]));
+        }
+        elseif($request->has('weight'))
+        {
+            $products->push(Product::all()->where('weight', '<=', $request['weight']));
+        }
+
+        if($products->count() == 0){
+
+           $products = Product::all();
+
+        }
+
+        return response()->json($products);
     }
 
 
